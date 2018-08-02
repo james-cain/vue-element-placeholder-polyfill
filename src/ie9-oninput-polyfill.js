9e42eba4e616ace37bc0d46937f0da06a703dbb9
james-cain <m15960192140@163.com>
2018-08-02 20:21:49 +0800

    fix: polyfill el 会读null  排除

--- src/ie9-oninput-polyfill.js
+++ src/ie9-oninput-polyfill.js
@@ -3,10 +3,10 @@
 
   d.addEventListener('selectionchange', function() {
     var el = d.activeElement;
-    if (el.tagName === 'TEXTAREA'
+    if (el && (el.tagName === 'TEXTAREA'
         || (el.tagName === 'INPUT' && el.type === 'text')
         || (el.tagName === 'INPUT' && el.type === 'password')
-        || (el.tagName === 'INPUT' && el.type === 'number')) {
+        || (el.tagName === 'INPUT' && el.type === 'number'))) {
       var ev = d.createEvent('CustomEvent');
       ev.initCustomEvent('input', true, true, {});
       el.dispatchEvent(ev);
